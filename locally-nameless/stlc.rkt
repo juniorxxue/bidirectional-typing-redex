fbf058264fefaa886e05da27da70d52d6fef3e12
Xu Xue <juniorxxue@gmail.com>
2020-12-28 21:13:58 +0800

    substitution metafunction of free and bound variables

--- locally-nameless/stlc.rkt
+++ locally-nameless/stlc.rkt
@@ -16,7 +16,7 @@
                            ((2 0) (1 0)))))))
 
 
-;; substitution that replace a free variable with a term
+;; substitution that replaces a free variable with a term
 (define-metafunction L
   subst : x e e -> e ;; match on e
   [(subst x e number) number]
@@ -27,3 +27,25 @@
   )
 
 (test-equal (term (subst Y Z (lambda base (0 Y)))) (term (lambda base (0 Z))))
+
+
+;; substitution that replaces a index with a term
+(define-metafunction L
+  open-rec : number e e -> e
+  [(open-rec number e number) e] ;; match
+  [(open-rec number_1 e number_2) number_2] ;; doesn't match
+  [(open-rec number e x) x]
+  [(open-rec number e_1 (lambda tau e_2)) (lambda tau (open-rec ,(+ (term number) 1) e_1 e_2))]
+  [(open-rec number e_1 (e_2 e_3)) ((open-rec number e_1 e_2) (open-rec number e_1 e_3))])
+
+(define-metafunction L
+  open-term : e e -> e
+  [(open-term e_1 e_2) (open-rec 0 e_2 e_1)])
+
+
+(term (open-term ((lambda base (1 0)) 0) Y))
+
+(test-equal (term (open-term ((lambda base (1 0)) 0) Y)) (term ((lambda base (Y 0)) Y)))
+
+
+;; ---- Local Closure ----
